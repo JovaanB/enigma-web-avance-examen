2bbe71671903cffa75d34620c1b3a121b8b24498
Jovan Bienvenu <jbienvenu@wisersoftware.com>
2021-06-14 07:08:56 +0200

    Add login form & validation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
-const App = () => <div>Hello, world.</div>;
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Login} from './pages/Login';
+import {Home} from './pages/Home';
+
+const App = () => (
+  <>
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  </>
+);
 
 export default App;
